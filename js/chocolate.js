53d17411af0b33c59d73868ba2ea5e419d9be62d
SeaTheHunters <153018362+SeaTheHunters@users.noreply.github.com>
2026-02-12 20:54:11 -0800

    Update chocolate.js

--- js/chocolate.js
+++ js/chocolate.js
@@ -1,9 +1,17 @@
 const choco = document.getElementById("choco");
 
 choco.addEventListener("click", () => {
-  choco.classList.add("melt");
+  choco.classList.add("crack");
+
+  setTimeout(() => {
+    choco.classList.add("break");
+  }, 600);
+
+  setTimeout(() => {
+    choco.classList.add("melt");
+  }, 1200);
 
   setTimeout(() => {
     window.location.href = "surprise.html";
-  }, 2200);
+  }, 2500);
 });
